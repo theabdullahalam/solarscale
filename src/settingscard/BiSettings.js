64c0174aa4196c576572cfd6d3d0cc12ff5b5972
Abdullah Alam <abdullahalam@protonmail.com>
2021-10-11 13:48:49 +0530

    dummy settings card

---
+++ src/settingscard/BiSettings.js
@@ -0,0 +1,15 @@
+import './BiSettings.scss'
+
+function BiSettings(props){
+    return (
+        <div className="BiSettings">
+            <div className="title">title</div>
+            <div className="buttons">
+                <button className="selected">option 1</button>
+                <button>option 2</button>
+            </div>            
+        </div>
+    )
+}
+
+export default BiSettings
--- src/settingscard/SettingsCard.js
+++ src/settingscard/SettingsCard.js
@@ -1,7 +1,13 @@
+import './SettingsCard.scss'
+
+import BiSettings from './BiSettings'
+
 function SettingsCard(props){
     return(
         <div className="SettingsCard">
-            <p>hola</p>
+            <h2>settings</h2>
+            <BiSettings />
+            <BiSettings />
         </div>
     )
 }
