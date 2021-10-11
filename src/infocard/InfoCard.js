d826176fe2801c048f2a5b8c3f5af49d71bff310
Abdullah Alam <abdullahalam@protonmail.com>
2021-10-11 13:24:17 +0530

    Add info card

--- src/infocard/InfoCard.js
+++ src/infocard/InfoCard.js
@@ -1,7 +1,17 @@
+import './InfoCard.scss'
+
 function InfoCard(props){
     return(
         <div className="InfoCard">
-            <p>infooooo</p>
+            <h1>solarscale</h1>
+            {/* <p>
+                Calculate Solar System sizes to a scale that you can set. 
+            </p> */}
+            <p>                
+                &#8226; Click on any object. <br />            
+                &#8226; Set a size for any of it's properties.<br />            
+                &#8226; The app will calculate everything else, to that scale.                
+            </p>
         </div>
     )
 }
