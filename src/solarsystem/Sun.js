13b860c52927f80a649687cde38ed2bc3bf6e0a0
Abdullah Alam <abdullahalam@protonmail.com>
2021-10-14 16:50:46 +0530

    show dummy card on sun click

--- src/solarsystem/Sun.js
+++ src/solarsystem/Sun.js
@@ -4,15 +4,28 @@ import './Sun.scss'
 import { getSensibleUnits } from './unitHelper'
 import { useState } from 'react'
 
+import {useSelector, useDispatch} from 'react-redux'
+
+import {
+    showCard,
+    selectCardState
+} from './valuesetter/valueSetterSlice'
+
 function Sun(props){
+
+    const dispatch = useDispatch()
 
     let sensibleSize = getSensibleUnits({
         value: props.radius,
         unit: 'cm'
     })
 
+    let openSetterCard = e => {
+        dispatch(showCard())
+    }
+
     return (
-        <div className="Sun" style={{backgroundColor: props.color}}>
+        <div className="Sun" style={{backgroundColor: props.color}} onClick={openSetterCard}>
             <div className="details">
                 <span className="title">{props.title}</span>
                 <span className="size">RADIUS: {sensibleSize.value} {sensibleSize.unit}</span>
